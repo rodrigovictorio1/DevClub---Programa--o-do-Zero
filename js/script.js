ec55eb065db8afa51f143e47a700cd8b25a7cd42
rodrigovictorio1 <rodrigovictorio.com@gmail.com>
2025-02-12 18:22:28 -0300

    add images and styling for DevClub Instagram page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let alunosDevClub = "João, Maria, Jorge, Pedro, Luiz, Paula, Anderson";
+let numeroDaCasa = 345;
+let carrinhoRodolfo = "Fone de Ouvido JBL";
+
+console.log(alunosDevClub);
+console.log(carrinhoRodrigo);
+
+function dizerMeuNome() {
+    console.log("Meu nome é: Rodrigo");
+}
+
+dizerMeuNome();
